5fe94e1d935a3513554b8abe8e5376bd19f44afa
justinjlukas1 <33607570+justinjlukas1@users.noreply.github.com>
2019-02-26 19:05:10 -0600

    Wrote results to excel file

--- web_scrape_test.py
+++ web_scrape_test.py
@@ -4,62 +4,68 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
+import xlsxwriter
 
 #launch url
 url = "https://www.target.com/p/nintendo-switch-with-neon-blue-and-neon-red-joy-con/-/A-52189185"
+
+#make dictionaries?
+element_titles = ['item name', 
+                  'store name', 
+                  'item price', 
+                  'store zip code', 
+                  'order by date', 
+                  'arrival date', 
+                  'shipping cost', 
+                  'store inventory']
+
+element_xpaths = ['//*[@id="mainContainer"]/div/div/div[1]/div[1]/div[1]/h1/span',
+                  '//*[@id="storeId-utilityNavBtn"]/div[2]',
+                  '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[1]/span',
+                  '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[1]/div[1]/div/div[1]/a',
+                  '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[1]/div[2]/p',
+                  '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[1]/div[2]/div/div/span',
+                  '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[2]/div/div[1]/div[1]/div[1]/div[1]',
+                  '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[2]/div/div[1]/div[1]/div[1]/div[2]'
+        ]
+
+element_list = []
+
+# Create a workbook and add a worksheet.
+workbook = xlsxwriter.Workbook('product_data.xlsx')
+worksheet = workbook.add_worksheet()
+
+row = 0
+col = 0
+
 
 # create a new Safari session
 driver = webdriver.Safari()
 driver.implicitly_wait(30)
 driver.get(url)
 
-try:
-    store_name_element = driver.find_element(By.XPATH, '//*[@id="storeId-utilityNavBtn"]/div[2]')
-    print(store_name_element.get_attribute('innerText'))
-except Exception:
-    print "There's no store name available"
-   
-try:
-    item_name_element = driver.find_element(By.XPATH, '//*[@id="mainContainer"]/div/div/div[1]/div[1]/div[1]/h1/span')
-    print(item_name_element.get_attribute('innerText'))
-except Exception:
-    print "There's no item name available"
-       
-try:
-    price_element = driver.find_element(By.XPATH, '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[1]/span')
-    print(price_element.get_attribute('innerText'))
-except Exception:
-    print "There's no pricce available"
-    
-try:
-    zip_code_element = driver.find_element(By.XPATH, '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[1]/div[1]/div/div[1]/a')
-    print(zip_code_element.get_attribute('innerText'))
-except Exception:
-    print "There's no zip code available"
-    
-try:
-    order_by_element = driver.find_element(By.XPATH, '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[1]/div[2]/p')
-    print(order_by_element.get_attribute('innerText'))
-except Exception:
-    print "There's no order by time available"
-    
-try:
-    arrival_date_element = driver.find_element(By.XPATH, '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[1]/div[2]/div/div/span')
-    print(arrival_date_element.get_attribute('innerText'))
-except Exception:
-    print "There's no arrival date available"
-    
-try:
-    shipping_cost_element = driver.find_element(By.XPATH, '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[2]/div/div[1]/div[1]/div[1]/div[1]')
-    print(shipping_cost_element.get_attribute('innerText'))
-except Exception:
-    print "There's no shipping cost available"
-   
-try:
-    current_inventory_element = driver.find_element(By.XPATH, '//*[@id="mainContainer"]/div/div/div[1]/div[2]/div/div[6]/div/div[2]/div/div[1]/div[1]/div[1]/div[2]')
-    print(current_inventory_element.get_attribute('innerText'))
-except Exception:
-    print "There's no current inventory available"
-    
+
+for i in range(0, len(element_titles)):
+    try:
+        temp_element = driver.find_element(By.XPATH, element_xpaths[i])
+        element_list.append(temp_element.get_attribute('innerText'))
+    except Exception:
+        try:
+            temp_element = driver.find_element(By.XPATH, element_xpaths[i])
+            element_list.append(temp_element.get_attribute('text'))
+        except Exception:
+            element_list.append(element_titles[i] + " not available")
+               
 driver.quit()
+
+for title in element_titles:
+    worksheet.write(row, col, title)
+    col += 1
+
+col = 0
+row += 1
+for element in element_list:
+    worksheet.write(row, col, element)
+    col += 1
+    
+workbook.close()
